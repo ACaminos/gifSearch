490c614f7a0efb625342aa35368a344ca2a22b0b
Ariel Caminos <caminosariel@gmail.com>
2024-03-23 20:50:15 -0300

    add hooks

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+
+  const aditional = () => {
+    setCategories( ['Valorant', ...categories ] ) // ---> Primera solucion
+    // setCategories( cat => [ ...cat, 'Valorant'] ) ---> Segunda solucion
+   }
+
   return (
     <>
         <h1>GifExpertApp</h1>
+
+
+        <ol>
+          { categories.map( category => (
+            <li key={ category }>{ category }</li>
+            ))}
+        </ol>
+
+        <button onClick={ aditional }>Agregar</button>
     </>
   )
 }
