3b23507a436ed06920dd064a9f15f9b6d4cfef35
Ariel <caminosariel@gmail.com>
2024-03-26 16:56:42 -0300

    fetch uncomplete

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export const GifGrid = ( { category } ) => {
+
+  const getGifs = async() => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=UKDssLvF2UM3gWSFMDMI8gsEdXRX0cpP&q=${ category }`
+    const resp = await fetch( url )
+
+    console.log( resp )
+  }
   return (
     <>
         <h3>{ category }</h3>
